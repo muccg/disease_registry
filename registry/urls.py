34e3f81b131e6c789003d28258fa49c9906b62ac
Andrew Ivins <aivins@ccg.murdoch.edu.au>
2012-09-18 17:12:24 +0800

    Added sma to development urls

--- registry/urls.py
+++ registry/urls.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     (r'^dm1/', include("ccg.django.app.dm1.urls"), {}),
-    (r'^dm1_questionairre/', include("ccg.django.app.dm1_questionairre.urls"), {}),
     (r'^dmd/', include("ccg.django.app.dmd.urls"), {}),
-    #(r'^sma/', include("ccg.django.app.sma.urls"), {}),
-    (r'^genetic/', include("ccg.django.app.genetic.urls"), {}),
+    (r'^sma/', include("ccg.django.app.sma.urls"), {}),
     (r'^admin/', include(admin.site.urls), {}),
 )
