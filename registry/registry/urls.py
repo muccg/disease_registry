6a0ab79fa62e4674244bdb29d0792ff2db12c723
bpower <bpower@ccg.murdoch.edu.au>
2012-12-04 13:11:13 +0800

    fixed template problems (relative paths)

--- registry/registry/urls.py
+++ registry/registry/urls.py
@@ -5,5 +5,10 @@ urlpatterns = patterns('',
     #(r'^dm1/', include("registry.dm1.urls"), {}),
     #(r'^dmd/', include("registry.dmd.urls"), {}),
     #(r'^sma/', include("registry.sma.urls"), {}),
+    (r'^genetic/', include("registry.genetic.urls"), {}),
+    (r'^groups/', include("registry.groups.urls"), {}),
+    (r'^humangenome/', include("registry.humangenome.urls"), {}),
+    (r'^patients/', include("registry.patients.urls"), {}),
+    
     (r'^admin/', include(admin.site.urls), {}),
 )
