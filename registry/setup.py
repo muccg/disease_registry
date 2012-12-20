f2bc314e8074312e9575fcd0997f771856920517
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2012-12-20 14:46:28 +0800

    making into an app

--- registry/setup.py
+++ registry/setup.py
@@ -2,8 +2,6 @@ import setuptools
 import os
 from setuptools import setup
 
-
-
 data_files = {}
 start_dir = os.getcwd()
 for package in ('patients', 'genetic', 'groups', 'humangenome'):
@@ -17,9 +15,10 @@ for package in ('patients', 'genetic', 'groups', 'humangenome'):
 setup(name='django-diseaseregistry',
     version='1.4',
     description='Django Disease Registry',
-    long_description='Collection Django applications implementing various disease registries',
+    long_description='Collection of Django applications implementing various aspects for disease registries',
     author='Centre for Comparative Genomics',
     author_email='web@ccg.murdoch.edu.au',
+    url="http://ccg.murdoch.edu.au",
     packages=[
         'registry',
         'registry.patients',
@@ -33,19 +32,9 @@ setup(name='django-diseaseregistry',
     package_data=data_files,
     zip_safe=False,
     install_requires=[
-        'Mango-py==1.3.1-ccg1-3',
-        'django-picklefield==0.1.9',
-        'django-templatetag-sugar==0.1',
-        'pyparsing==1.5.6',
-        'wsgiref==0.1.2',
-        'python-memcached==1.48',
         'Mako>=0.5.0',
         'South>=0.7.3',
-        'django-extensions>=0.7.1',
-        'ccg-auth==0.3.2',
-        'ccg-extras==0.1.5',
         'ccg-makoloader==0.2.4',
-        'django-userlog==0.2.1',
     ],
 )
 
