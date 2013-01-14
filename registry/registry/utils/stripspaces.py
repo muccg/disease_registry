5fd818369aeb1d1f6d9fa06280bee295557a8569
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2013-01-14 15:48:19 +0800

    simplified stripspaces, as used in warda

--- registry/registry/utils/stripspaces.py
+++ registry/registry/utils/stripspaces.py
@@ -1,19 +1,13 @@
 # Utility function to clean a string
 def stripspaces(s):
-	"""
-	Remove whitespace chars from both ends of the string
-	returns a new string with whitespace chars removed from both ends of the string
-	converts multiple whitespace chars into 1 space char
-	returns an empty string if input is None or anything else than a string or unicode
-	TODO: change the re expression to strip the start and end whitespace as well and remove the strip() call
-	"""
-	import re
-	if s == None or not isinstance(s, basestring):
-		return ''
-	else:
-		t = s.strip()   # trip whitespaces chars from both ends
-		r = re.sub(r'[\s\s]+', ' ', t)  # convert multiple whitespace chars into one
-	return r
+    """Remove whitespace chars from both ends of the string returns a new
+    string with whitespace chars removed from both ends of the string
+    converts multiple whitespace chars into 1 space char returns an empty
+    string if input is None or anything else than a string or unicode.
+    """
+    if s == None or not isinstance(s, basestring):
+        return ""
+    return " ".join(s.strip().split())
 
 #to run the tests:
 #python stripspaces.py
