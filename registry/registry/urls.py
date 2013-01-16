4abf9f2f16b55085d864d773afe2f93097add97b
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2013-01-16 14:59:33 +0800

    tidy use of ccg url

--- registry/registry/urls.py
+++ registry/registry/urls.py
@@ -2,13 +2,10 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    #(r'^dm1/', include("registry.dm1.urls"), {}),
-    #(r'^dmd/', include("registry.dmd.urls"), {}),
-    #(r'^sma/', include("registry.sma.urls"), {}),
     (r'^genetic/', include("registry.genetic.urls"), {}),
     (r'^groups/', include("registry.groups.urls"), {}),
     (r'^humangenome/', include("registry.humangenome.urls"), {}),
     (r'^patients/', include("registry.patients.urls"), {}),
     
-    (r'^admin/', include(admin.site.urls), {}),
+    # (r'^admin/', include(admin.site.urls), {}),
 )
