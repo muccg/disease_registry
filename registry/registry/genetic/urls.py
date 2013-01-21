c9bae702b223aaf13c143b077cb5658f098c3fca
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-01-21 15:39:28 +0800

    django.conf.urls.defaults is deprecated in Django 1.4. Starting with Django 1.4 functions patterns, url, include plus the handler* symbols in the django.conf.urls module. They will be removed in 1.6.

--- registry/registry/genetic/urls.py
+++ registry/registry/genetic/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 
 urlpatterns = patterns("",
     url(r"^variation/", "registry.genetic.views.entry", {}, name="entry"),
