0b678eec6ab64a143e128bdadffc4ad07a5f4990
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-01-29 14:10:21 +0800

    Removed redundant default empty dicts.

--- registry/registry/genetic/urls.py
+++ registry/registry/genetic/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
+import views
 
 urlpatterns = patterns("",
-    url(r"^variation/", "registry.genetic.views.entry", {}, name="entry"),
+    url(r"^variation/", views.entry, name="entry"),
 )
--- registry/registry/urls.py
+++ registry/registry/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^genetic/', include("registry.genetic.urls"), {}),
-    url(r'^groups/', include("registry.groups.urls"), {}),
-    url(r'^humangenome/', include("registry.humangenome.urls"), {}),
-    url(r'^patients/', include("registry.patients.urls"), {}),
-    url(r'^errortest/', include("registry.common.urls"), {}),
+    url(r'^genetic/', include("registry.genetic.urls")),
+    url(r'^groups/', include("registry.groups.urls")),
+    url(r'^humangenome/', include("registry.humangenome.urls")),
+    url(r'^patients/', include("registry.patients.urls")),
+    url(r'^errortest/', include("registry.common.urls")),
 )
