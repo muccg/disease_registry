3729a40b2611eacb6b57401b995a00f7e61b7d80
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-03-21 15:39:28 +0800

    Initial implementation of django_qbe on DMD. Issue RDR-26

--- registry/registry/groups/models.py
+++ registry/registry/groups/models.py
@@ -1,6 +1,6 @@
 from django.db import models, transaction
 
-import django.contrib.auth
+from django.contrib.auth.models import User
 
 
 class WorkingGroup(models.Model):
@@ -14,7 +14,7 @@ class WorkingGroup(models.Model):
 
 
 class User(models.Model):
-    user = models.OneToOneField(django.contrib.auth.models.User, primary_key=True)
+    user = models.OneToOneField(User, primary_key=True)
     title = models.CharField(max_length=50, verbose_name="position")
     working_group = models.ForeignKey(WorkingGroup, null=True)
 
@@ -40,4 +40,4 @@ from django.db.models.signals import post_save
 def user_post_save_handler(sender, instance, **kwargs):
     profile, new = User.objects.get_or_create(user=instance)
 
-post_save.connect(user_post_save_handler, sender=django.contrib.auth.models.User)
+post_save.connect(user_post_save_handler, sender=User)
