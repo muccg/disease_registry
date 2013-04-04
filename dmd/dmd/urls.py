c83f652d371573e3e1478b0a07f26f7a997fbfa0
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-04-04 09:37:03 +0800

    DMD removed url reference to HTSQL query engine

--- dmd/dmd/urls.py
+++ dmd/dmd/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'', include("dmd.dmd.urls")),
     (r'', include(common_urls, namespace="registry")),
-    (r'^htsql/', include('htsql_django.urls')),
     (r'^qbe/', include('django_qbe.urls')),
 )
 
