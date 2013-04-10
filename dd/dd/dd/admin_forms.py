f2ea8f727327a49063a999d624a9b5c35bd0ed21
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-04-10 14:05:53 +0800

    Merge with next_release

---
+++ dd/dd/dd/admin_forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from models import *
+from registry.forms.widgets import ComboWidget, LiveComboWidget, StaticWidget, FVCPercentageWidget, PercentageWidget
+from registry.forms.date import DateWidget
+
+from django.forms import Select
+from django.forms.models import modelformset_factory, inlineformset_factory
+from django.forms.widgets import RadioSelect
+
+class DDMedicalHistoryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(DDMedicalHistoryForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = DDMedicalHistoryRecord
+        widgets = {
+                "other" : forms.Textarea(attrs={"cols":60, "rows":3}),
+        }
+
+class DDDiagnosisForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(DDDiagnosisForm, self).__init__(*args, **kwargs)
+        
+        if "instance" in kwargs:
+            self.fields["patient"] = forms.ModelChoiceField(Patient.objects.all(), widget=StaticWidget(text=unicode(kwargs["instance"])))
+        else:
+            import registry.groups.models
+            user = registry.groups.models.User.objects.get(user=self.user)
+            if self.user.is_superuser:
+                self.fields["patient"] = forms.ModelChoiceField(Patient.objects.all())
+            else:
+                self.fields["patient"] = forms.ModelChoiceField(Patient.objects.filter(working_group=user.working_group).filter(active=True))
+
+LabDataFormset = inlineformset_factory(LabData, LabDataRecord, extra=0, can_delete=False)
+class DDLabDataForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        print 'ddlabdataform inited'
+        super(DDLabDataForm, self).__init__(*args, **kwargs)
+        self.labdatarecord_formset = LabDataFormset(instance = self.instance, data = self.data or None, prefix = self.prefix)
+    def is_valid(self):
+        return (super(DDLabDataForm, self).is_valid() and self.labdatarecord_formset.is_valid() )
+
+    def save(self, commit = True):
+        assert commit == True
+        res = super(DDLabDataForm, self).save(commit=commit)
+        self.labdararecord_formset.save()
+        return res
---
+++ dd/dd/dd/models.py
@@ -0,0 +1,427 @@
+import traceback
+import datetime
+from django.db import models
+from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+#from registry.genetic.models import MolecularData
+from registry.patients.models import Patient as RegistryPatient
+
+import logging
+logger = logging.getLogger('registry_log')
+
+class Patient(RegistryPatient):
+    SEX_CHOICES = ( ("M", "Male"), ("F", "Female"), ("X", "Other/Intersex") )
+
+    place_of_birth = models.CharField(max_length=100, null=True, blank=True, verbose_name="Place Of Birth")
+
+#Longitudinal sets will have many instances of longitudinal data
+#pointing at them through a foreign key relationship.
+#These instances should have a date field (part of the longitudinal data class)
+#These instances should have the fk relation have a related name of 'longitudinal_series'
+#For example:
+#
+#
+#class ExampleRecord(LongitudinalData):
+#    record_history = models.ForeignKey(LongitudinalSet, related_name='longitudinal_series')
+#
+class LongitudinalSet(models.Model):
+    def currentRecord(self):
+        pass
+
+    def previousRecord(self, recordid):
+        retval = None
+        records = self.dataset()
+        ids = [record.id for record in records]
+        if recordid in ids:
+            precordindex = ids.index(recordid)-1
+            if precordindex >= 0:
+                retval = records[precordindex]
+        return retval
+
+    def nextRecord(self, recordid):
+        retval = None
+        records = self.dataset()
+        ids = [record.id for record in records]
+        if recordid in ids:
+            precordindex = ids.index(recordid)+1
+            if precordindex > len(records):
+                retval = records[precordindex]
+        return retval
+
+    def dataset(self):
+        try:
+            return self.longitudinal_series.order_by('date')
+        except:
+            return []
+
+    def __unicode__(self):
+        ret = "Empty"
+        records = self.dataset()
+        if len(records):
+            if len(records) > 1:
+                ret = "%s-%s" % (records[0].date, records[-1].date)
+            else:
+                #only one record
+                ret = "%s-" % (records[0].date)
+        return "(%s)" % (ret)
+
+class OrphanetChoices(models.Model):
+    code = models.CharField(max_length=6)
+    description = models.CharField(max_length=120) # need over 104, otherwise the SQL import fails
+
+    class Meta:
+        verbose_name = "Orphanet Code"
+        ordering = ["code"] # doesn't seem to work on subclasses
+
+    def __unicode__(self):
+        return "%s - %s" % (self.code, self.description) # used in the select options
+
+
+
+#inherit from this to create longitudinal data
+#your inheriting class should have a foreignkey field to a
+#descendant of LongitudinalSet, and name the reverse relation 'longitudinal_series'
+class LongitudinalData(models.Model):
+    date = models.DateField()
+    #longitudinal_set = models.ForeignKey(LongitudinalSet)
+
+
+class MedicalHistoryDisease(models.Model):
+    disease = models.CharField(max_length = 100)
+    chronic = models.BooleanField(default = False, verbose_name = "Chronic / incurable")
+    
+    def __unicode__(self):
+        return str(self.disease)
+
+    class Meta:
+        verbose_name = "Medical History Disease"
+        verbose_name_plural = "Medical History Diseases"
+
+class MedicalHistory(LongitudinalSet):
+    patient = models.ForeignKey(RegistryPatient)
+    disease = models.ManyToManyField(MedicalHistoryDisease)
+    
+    def __unicode__(self):
+        return str(self.patient)
+
+    class Meta:
+        verbose_name = "Medical History"
+        verbose_name_plural = "Medical Histories"
+    
+class MedicalHistoryRecord(LongitudinalData):
+    medical_history = models.ForeignKey(MedicalHistory, related_name='longitudinal_series')
+
+    def __unicode__(self):
+        return "%s (%s)" % (str(self.medical_history.patient), str(self.date) )
+
+class LabData(LongitudinalSet):
+    patient = models.ForeignKey(RegistryPatient)
+
+
+class LabDataRecord(LongitudinalData):
+    labdata_history = models.ForeignKey(LabData, related_name='longitudinal_series')
+    def __unicode__(self):
+        return "%s (%s)" % (str(self.labdata_history.patient), str(self.date) )
+
+class MRIData(LongitudinalSet):
+    patient = models.ForeignKey(RegistryPatient)
+
+    def __unicode__(self):
+        return str(self.patient)
+
+class MRIDataRecord(LongitudinalData):
+    mri_history = models.ForeignKey(MRIData, related_name = 'logitudinal_series')
+
+class TreatmentOverview(models.Model):
+    patient = models.ForeignKey(RegistryPatient)
+
+    def __unicode__(self):
+        return str(self.patient)
+
+class Treatment(models.Model):
+    #overview = models.ForeignKey(TreatmentOverview)
+    name = models.CharField(max_length = 100)
+    common_name = models.CharField(max_length = 100)
+
+    def __unicode__(self):
+        if self.common_name:
+            return "%s (%s)" % (self.name, self.common_name)
+        else:
+            return "%s" % (self.name)
+
+class TreatmentCourse(models.Model):
+    treatment = models.ForeignKey(Treatment)
+    overview = models.ForeignKey(TreatmentOverview)
+    start_date = models.DateField()
+    end_date = models.DateField(blank = True)
+    dose = models.TextField()
+    notes = models.TextField(blank = True)
+
+
+class DDDiagnosis(models.Model):
+
+    DD_AFFECTED_STATUS_CHOICES = (
+        ('FamilyHistory', 'Not yet diagnosed/Family history only'),
+        ('AS2', 'DD Affected Status 2'),
+        ('AS3', 'DD Affected Status 3'),
+        ('AS4', 'DD Affected Status 4'),
+    )
+    DD_DIAGNOSIS_CHOICES = (
+        ("UNK", "Unknown"),
+        ("DD1", "Demyelinating Disease 1"),
+        ("DD2", "Demyelinating Disease 2"),
+    )
+    
+    DD_FIRST_SUSPECTED_CHOICES = (
+        ("NA", "Not Applicable"), # Trac 16 #61
+        ("Self", "Self"),
+        ("Family", "Family"),
+        ("GP", "GP"),
+        ("Paediatrician", "Paediatrician"),
+        ("Neurologist", "Neurologist"),
+        ("Cardiologist", "Cardiologist"),
+        ("Ophthalmologist", "Ophthalmologist"),
+        ("Geneticist", "Geneticist"),
+        ("Other", "Other"),
+    )
+    patient = models.OneToOneField(Patient, primary_key=True, related_name='patient_diagnosis')
+    diagnosis = models.CharField(max_length=3, choices = DD_DIAGNOSIS_CHOICES, verbose_name = "Condition", default = 'UNK')
+    affected_status = models.CharField(max_length=30, choices=DD_AFFECTED_STATUS_CHOICES, verbose_name = "Affected Status", default = '')
+
+    first_suspected_by = models.CharField(max_length=50, choices = DD_FIRST_SUSPECTED_CHOICES, null=True, blank=True)
+
+    age_at_clinical_diagnosis = models.IntegerField('age in years at clinical diagnosis', null=True, blank=True)
+    age_at_molecular_diagnosis = models.IntegerField('age in years at molecular diagnosis', null=True, blank=True)
+
+    orphanet = models.ForeignKey(OrphanetChoices, null=True, blank = True)
+    
+    created = models.DateTimeField(editable=False)
+    updated = models.DateTimeField(editable=False)
+
+    def __unicode__(self):
+        return str(self.patient)
+
+    class Meta:
+        verbose_name = "Demyelinating Disease Diagnosis"
+        verbose_name_plural = "Demyelinating Disease Diagnoses"
+
+    def save(self, *args, **kwargs):
+        '''On save, update timestamps, auto-fields reportedly unreliable'''
+        if not self.created:
+            self.created = datetime.datetime.now()
+        self.updated = datetime.datetime.now()
+        super(DDDiagnosis, self).save(*args, **kwargs)
+
+    def percentage_complete(self):
+        score = 0.0
+        fieldlist = ['affected_status']
+        for f in fieldlist:
+            try:
+                getattr(self, f)
+                score += 1.0
+            except ObjectDoesNotExist, e:
+                pass
+        return  int(score / len(fieldlist) * 100.0)
+
+    def incomplete_sections(self):
+        fieldlist = ['affected_status']
+        fields_to_complete = []
+        for f in fieldlist:
+            try:
+                getattr(self, f)
+            except ObjectDoesNotExist, e:
+                fields_to_complete.append(f)
+
+        if fields_to_complete:
+            return "Incomplete sections: %s." % ", ".join(fields_to_complete)
+        else:
+            return "All sections complete."
+
+    def progress_graph(self):
+        """This strictly speaking should be on the admin as it is used there,
+        but if included on the model we can access it from registry.patient
+        as well as from the diagnosis admin
+        """
+        graph_html = '<img title="%s" src="http://chart.apis.google.com/chart' % self.incomplete_sections()
+        graph_html += '?chf=bg,s,FFFFFF00&chs=200x15&cht=bhs&chco=4D89F9,C6D9FD&chd=t:%d|100&chbh=5"/>' % self.percentage_complete()
+        return graph_html
+
+class DDMedicalHistoryRecord(MedicalHistoryRecord):
+    diagnosis                    = models.ForeignKey(DDDiagnosis, null=True, blank=True)
+    diabetes                     = models.BooleanField(default = False, verbose_name="Diabetes")
+    diabetes_insulin             = models.BooleanField(default = False, verbose_name="If yes, do you use insulin?")
+    diabetes_onset_age           = models.IntegerField(default = 1,     verbose_name = "Age of onset")
+    rheumatoid_arthritis         = models.BooleanField(default = False, verbose_name = "Rheumatoid Arthritis")
+    crohns_disease               = models.BooleanField(default = False, verbose_name = "Crohn's Disease")
+    ulcerative_colitis           = models.BooleanField(default = False, verbose_name = "Ulcerative Colitis")
+    psoriasis                    = models.BooleanField(default = False, verbose_name = "Psoriasis")
+    myasthenia_gravis            = models.BooleanField(default = False, verbose_name = "Myasthenia Gravis")
+    vitiligo                     = models.BooleanField(default = False, verbose_name = "Vitiligo")
+    thyroid_disease              = models.BooleanField(default = False, verbose_name = "Thyroid Disease")
+    thyroid_hypothyroidism       = models.BooleanField(default = False, verbose_name = "Hypothyroidism")
+    thyroid_hashimotos           = models.BooleanField(default = False, verbose_name = "Hashimoto's Thyroiditis")
+    graves_disease               = models.BooleanField(default = False, verbose_name = "Graves' Disease")
+    sjogrens_syndrome            = models.BooleanField(default = False, verbose_name = "Sjorgens Syndrome")
+    pernicious_anemia            = models.BooleanField(default = False, verbose_name = "Pernicious Anemia")
+    systemic_lupus_erythematosus = models.BooleanField(default = False, verbose_name = "Systemic Lupus Erythematosus")
+    alopecia                     = models.BooleanField(default = False, verbose_name = "Alopecia")
+    family_history_of_ms         = models.BooleanField(default = False, verbose_name = "Family history of MS, NMO (or other autoimmune disease)")
+    other                        = models.TextField(verbose_name="Other", blank=True)
+
+    class Meta:
+        verbose_name = "Medical History Record"
+        verbose_name_plural = "Medical History Records"
+
+class DDClinicalData(models.Model):
+    ''' The full text of EDSS ratings. Possibly for use in a pop up widget - dropdown descriptions have been shortened for layout purposes.
+    (0.0, "Normal Neurological Exam"),
+    (1.0, "No disability, minimal signs on 1 FS"),
+    (1.5, "No disability, minimal signs on 2 of 7 FS"),
+    (2.0, "Minimal disability in 1 of 7 FS"),
+    (2.5, "Minimal disability in 2 FS"),
+    (3.0, "Moderate disability in 1 FS; or mild disability in 3 - 4 FS, though fully ambulatory"),
+    (3.5, "Fully ambulatory but with moderate disability in 1 FS and mild disability in 1 or 2 FS; or moderate disability in 2 FS; or mild disability in 5 FS"),
+    (4.0, "Fully ambulatory without aid, up and about 12hrs a day despite relatively severe disability. Able to walk without aid 500 meters"),
+    (4.5, "Fully ambulatory without aid, up and about much of day, able to work a full day, may otherwise have some limitations of full activity or require minimal assistance. Relatively severe disability. Able to walk without aid 300 meters"),
+    (5.0, "Ambulatory without aid for about 200 meters. Disability impairs full daily activities"),
+    (5.5, "Ambulatory for 100 meters, disability precludes full daily activities"),
+    (6.0, "Intermittent or unilateral constant assistance (cane, crutch or brace) required to walk 100 meters with or without resting"),
+    (6.5, "Constant bilateral support (cane, crutch or braces) required to walk 20 meters without resting"),
+    (7.0, "Unable to walk beyond 5 meters even with aid, essentially restricted to wheelchair, wheels self, transfers alone; active in wheelchair about 12 hours a day"),
+    (7.5, "Unable to take more than a few steps, restricted to wheelchair, may need aid to transfer; wheels self, but may require motorized chair for full day's activities"),
+    (8.0, "Essentially restricted to bed, chair, or wheelchair, but may be out of bed much of day; retains self care functions, generally effective use of arms"),
+    (8.5, "Essentially restricted to bed much of day, some effective use of arms, retains some self care functions"),
+    (9.0, "Helpless bed patient, can communicate and eat"),
+    (9.5, "Unable to communicate effectively or eat/swallow"),
+    (10.0, "Death due to MS")
+    '''
+    
+    EDSSRatingChoices = ( ('0.0', "Normal Neurological Exam"),
+                          ('1.0', "No disability, minimal signs on 1 FS"),
+                          ('1.5', "No disability, minimal signs on 2 of 7 FS"),
+                          ('2.0', "Minimal disability in 1 of 7 FS"),
+                          ('2.5', "Minimal disability in 2 FS"),
+                          ('3.0', "Fully amb (mod dblty in 1 FS; or mild dblty in 3-4 FS)"),
+                          ('3.5', "Fully amb but with mod/mild disability in several FS"),
+                          ('4.0', "Fully amb w/out aid 500m"),
+                          ('4.5', "Fully amb w/out aid 300m"),
+                          ('5.0', "Amb w/out aid 200m. Impaired activity"),
+                          ('5.5', "Amb 100m, daily activities impaired"),
+                          ('6.0', "Amb 100m with unilateral assist"),
+                          ('6.5', "Amb 20m with bilateral assist"),
+                          ('7.0', "Amb <5m w/out wheelchair"),
+                          ('7.5', "Wheelchair restricted, mainly self propelled"),
+                          ('8.0', "Bed/chair/wheelchair restricted, self care"),
+                          ('8.5', "Bed restricted, some arm use/self care"),
+                          ('9.0', "Helpless bed patient, can communicate and eat"),
+                          ('9.5', "Unable to communicate effectively or eat/swallow"),
+                          ('10.0', "Death due to MS")
+                        )
+
+    diagnosis               = models.ForeignKey(DDDiagnosis)
+    date                    = models.DateField(verbose_name = "Clinical Data date")
+    date_first_symtoms      = models.DateField(verbose_name = "Date of first symptoms")
+    edss_rating             = models.CharField(max_length=4, choices=EDSSRatingChoices)
+    past_medical_history    = models.ForeignKey(DDMedicalHistoryRecord, null=True, blank = True)
+    date_of_visits          = models.DateField(verbose_name = "Date of visits")
+
+    def __unicode__(self):
+        return str(self.diagnosis)
+
+    class Meta:
+        verbose_name = "Clinical Data"
+        verbose_name_plural = "Clinical Data"
+
+class DDLabDataRecord(LabDataRecord):
+    protein =           models.FloatField(default = 0.0, verbose_name = "Protein (g/L)")
+    leucocytes =        models.FloatField(default = 0.0, verbose_name = "Leucocytes (/ul)")
+    erythrocytes =      models.FloatField(default = 0.0, verbose_name = "Erythrocytes (/ul)")
+    oligoclonal_bands = models.FloatField(default = 0.0, verbose_name = "Oligoclonal Bands")
+    igg_alb =           models.FloatField(default = 0.0, verbose_name = "IgG/Alb")
+
+
+class DDMRIDataRecord(MRIDataRecord):
+    cds_available = models.BooleanField(default = False, verbose_name = "CDs Available")
+    brain = models.BooleanField(default = False, verbose_name = "Brain")
+    spinal_cord = models.BooleanField(default = False, verbose_name = "Spinal Cord")
+    cervical = models.BooleanField(default = False, verbose_name = "Cervical")
+    thoracic = models.BooleanField(default = False, verbose_name = "Thoracic")
+
+
+class DDMRIData(MRIData):
+    diagnosis = models.ForeignKey(DDDiagnosis, null=True, blank = True)
+    class Meta:
+        verbose_name = "MRI Data"
+        verbose_name_plural = "MRI Data"
+
+class DDLabData(LabData):
+    diagnosis = models.ForeignKey(DDDiagnosis, null=True, blank = True)
+
+    def __unicode__(self):
+        return str(self.diagnosis)
+
+    class Meta:
+        verbose_name = "Lab Data"
+        verbose_name_plural = "Lab Data"
+
+class DDTreatmentOverview(TreatmentOverview):
+    diagnosis = models.ForeignKey(DDDiagnosis, null=True, blank = True)
+    treatments = models.ManyToManyField(Treatment, null=True, blank = True)
+
+    def populate_initial_items(self):
+        initial_items = [(u'1FN\u03b20 1a', "Avonex"), 
+                         (u'1FN\u03b2 1a', "Rebif"),
+                         (u'1FN\u03b2 1b', "Betaferon"),
+                         (u'Natalizumab', "Tysabri"),
+                         (u'Glatiramer Acetate', "Copaxone"),
+                         (u'Fingolimod', "Gilenya"),
+                         (u'Mitoxantrone', None),
+                         (u'Cladribine', None),
+                         (u'Cyclophosphamide', None),
+                         (u'Oral Steroids', None),
+                         (u'Bone Marrow', None),
+                         (u'Transplants', None),
+                         (u'Rituximab', None),
+                         (u'Alemtuzumab', None),
+                         ]
+        for item in initial_items:
+            t, created = Treatment.objects.get_or_create(name = item[0])
+            if created and item[1]:
+                t.common_name = item[1]
+            #t.overview = self
+                t.save()
+
+            self.treatments.add(t)
+
+    def save(self, *args, **kwargs):
+        created = False
+        if not self.pk:
+            created = True
+        super(DDTreatmentOverview, self).save(*args, **kwargs)
+        if created:
+            self.populate_initial_items()
+
+    class Meta:
+        verbose_name = "Treatment Overview"
+        verbose_name_plural = "Treatment Overviews"
+
+
+def signal_patient_post_save(sender, **kwargs):
+    logger.debug("patient post_save signal")
+
+    try:
+        patient = kwargs['instance']
+        diagnosis, created = DDDiagnosis.objects.get_or_create(patient=patient)
+        logger.debug("Diagnosis record %s" % ("created" if created else "already existed"))
+    except Exception, e:
+        logger.critical(e)
+        logger.critical(traceback.format_exc())
+        raise
+
+
+# connect up django signals
+post_save.connect(signal_patient_post_save, sender=Patient)
+
+
+
+
