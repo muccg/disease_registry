0cf49b448c4b26a52a3e2eae109bd265e6c71cd1
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-05-16 09:45:47 +0800

    dmd tests: update dmd report test

---
+++ dd/dd/dd/admin.py
@@ -0,0 +1,134 @@
+from django.contrib import admin
+from dd.dd.models import *
+from admin_forms import *
+from registry.utils import get_static_url
+from registry import groups
+
+class ClinicalDataAdmin(admin.ModelAdmin):
+    model = DDClinicalData
+    form = ClinicalDataForm
+
+class DDMedicalHistoryAdminInline(admin.TabularInline):
+    model = DDMedicalHistoryRecord
+    extra = 1
+    form = DDMedicalHistoryForm
+
+class MedicalHistoryAdmin(admin.ModelAdmin):
+    inlines = [DDMedicalHistoryAdminInline]
+
+class LabDataInline(admin.TabularInline):
+    model = LabData
+    excludes = ('patient',)
+
+class LabDataAdmin(admin.ModelAdmin):
+    model = LabData
+
+class TreatmentCourseInline(admin.TabularInline):
+    model = TreatmentCourse
+    form = TreatmentCourseForm
+
+class TreatmentInline(admin.TabularInline):
+    model = Treatment
+
+class TreatmentAdmin(admin.ModelAdmin):
+    inlines = [TreatmentCourseInline]
+
+class DDTreatmentOverviewAdmin(admin.ModelAdmin):
+    model = DDTreatmentOverview
+    inlines = [TreatmentCourseInline]
+
+class DDTreatmentOverviewInline(admin.StackedInline):
+    model = DDTreatmentOverview
+
+class DDClinicalDataInline(admin.TabularInline):
+    model = DDClinicalData
+
+class MRIFileInline(admin.TabularInline):
+    model = MRIFile
+    extra = 0
+
+class MRIDataInline(admin.StackedInline):
+    model = MRIData
+    inlines = [MRIFileInline]
+    fields = ("diagnosis", "date", "location",
+              ("brain", "cervical", "thoracic"),
+              "report_file", "image_file")
+    form = MRIDataForm
+    extra = 1
+    change_form_template = "progressbarupload/change_form.html"
+    add_form_template = "progressbarupload/change_form.html"
+
+class MRIDataAdmin(admin.ModelAdmin):
+    inlines = [MRIFileInline]
+    fields = ("diagnosis", "date", "location",
+              ("brain", "cervical", "thoracic"),
+              "report_file")
+    change_form_template = "progressbarupload/change_form.html"
+    add_form_template = "progressbarupload/change_form.html"
+
+class DDDiagnosisAdmin(admin.ModelAdmin):
+    form = DDDiagnosisForm
+
+    inlines = [
+               DDMedicalHistoryAdminInline,
+               DDClinicalDataInline,
+               LabDataInline,
+               DDTreatmentOverviewInline,
+               MRIDataInline,
+              ]
+
+    search_fields = ["patient__family_name", "patient__given_names"]
+    list_display = ['patient_name', 'patient_working_group', 'progress_graph']
+
+    def patient_name(self, obj):
+        return ("%s") % (obj.patient,)
+
+    def patient_working_group(self, obj):
+        return ("%s") % (obj.patient.working_group,)
+
+    patient_name.short_description = 'Name'
+    patient_working_group.short_description = 'Working Group'
+
+    class Media:
+        css = {
+            "screen": [get_static_url("css/diagnosis_admin.css")]
+        }
+
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return Diagnosis.objects.all()
+
+        user = groups.models.User.objects.get(user=request.user)
+
+        if self.has_change_permission(request):
+            return Diagnosis.objects.filter(patient__working_group=user.working_group).filter(patient__active=True)
+        else:
+            return Diagnosis.objects.none()
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        We provide our own get_form so we can access the user object
+        and narrow choice fields in the form by user rights
+        """
+        form = super(DDDiagnosisAdmin, self).get_form(request, obj, **kwargs)
+        form.user = request.user
+        return form
+
+    def progress_graph(self, obj):
+        return obj.progress_graph()
+
+    progress_graph.allow_tags = True
+    progress_graph.short_description = "Diagnosis Entry Progress"
+
+class MedicalHistoryDiseaseAdmin(admin.ModelAdmin):
+    list_display = ['disease',]
+
+admin.site.register(MedicalHistory, MedicalHistoryAdmin)
+admin.site.register(LabData, LabDataAdmin)
+admin.site.register(Treatment, TreatmentAdmin)
+admin.site.register(DDTreatmentOverview, DDTreatmentOverviewAdmin)
+admin.site.register(Diagnosis, DDDiagnosisAdmin)
+admin.site.register(DDClinicalData, ClinicalDataAdmin)
+admin.site.register(MRIData, MRIDataAdmin)
+admin.site.register(MedicalHistoryDisease, MedicalHistoryDiseaseAdmin)
+admin.site.register(EdssRating)
---
+++ dd/dd/dd/admin_forms.py
@@ -0,0 +1,102 @@
+from django import forms
+from models import *
+from registry.forms.widgets import ComboWidget, LiveComboWidget, StaticWidget, FVCPercentageWidget, PercentageWidget
+from registry.forms.date import DateWidget
+
+from django.forms import Select
+from django.forms.models import modelformset_factory, inlineformset_factory
+from registry.forms.widgets import NoDotsRadioSelect
+
+class ClinicalDataForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ClinicalDataForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = DDClinicalData
+        widgets = {
+            'edss_evaluation_type': NoDotsRadioSelect()
+        }
+
+class DDMedicalHistoryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(DDMedicalHistoryForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = DDMedicalHistoryRecord
+        widgets = {
+                "other" : forms.Textarea(attrs={"cols":30, "rows":4}),
+        }
+
+class DDDiagnosisForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(DDDiagnosisForm, self).__init__(*args, **kwargs)
+
+        if "instance" in kwargs:
+            self.fields["patient"] = forms.ModelChoiceField(Patient.objects.all(), widget=StaticWidget(text=unicode(kwargs["instance"])))
+        else:
+            import registry.groups.models
+            user = registry.groups.models.User.objects.get(user=self.user)
+            if self.user.is_superuser:
+                self.fields["patient"] = forms.ModelChoiceField(Patient.objects.all())
+            else:
+                self.fields["patient"] = forms.ModelChoiceField(Patient.objects.filter(working_group=user.working_group).filter(active=True))
+
+class TreatmentCourseForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TreatmentCourseForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        CHOICES = [
+            ('S','Standard'),
+            ('O','Other')
+        ]
+
+        model = TreatmentCourse
+        widgets = {
+            'dose_other': forms.Textarea(attrs={"cols": 35, "rows": 5}),
+            'notes': forms.Textarea(attrs={"cols": 35, "rows": 5}),
+            'dose_type': NoDotsRadioSelect(choices=CHOICES)
+        }
+
+class MRIDataForm(forms.ModelForm):
+    """
+    This form is for the inline MRIData admin. It allows uploading a
+    single image file through the image_file field. image_file is
+    mapped to a MRIFile object which refers to MRIData.
+    """
+    class Meta:
+        model = MRIData
+
+    def __init__(self, instance=None, *args, **kwargs):
+        kwargs["instance"] = instance
+        super(MRIDataForm, self).__init__(*args, **kwargs)
+
+        # Initialize image_field field with last object in the
+        # instance images set.
+        if instance:
+            images = instance.images.all()
+            if len(images) > 0:
+                image = images[len(images) - 1].image
+                self.fields["image_file"].initial = image
+
+    def save(self, commit=True):
+        instance = super(MRIDataForm, self).save(commit)
+        instance.save()
+
+        if "image_file" in self.cleaned_data:
+            image = self.cleaned_data["image_file"]
+            if image:
+                # If an image file is uploaded, add it on to the end
+                # of the images set.
+                mri = MRIFile(data=instance, image=image)
+                mri.save()
+            elif image is False:
+                # User selected "Clear" => delete the last object in
+                # the instance images set.
+                images = instance.images.all()
+                if len(images) > 0:
+                    images[len(images) - 1].delete()
+
+        return instance
+
+    image_file = forms.FileField(required=False)
