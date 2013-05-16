d8750198e1cd8ec8785b9ef4289484a7bfe6e423
Adam Hunter <aahunter@gmail.com>
2013-05-16 15:08:26 +0800

    different port for apps

--- develop.sh
+++ develop.sh
@@ -7,6 +7,11 @@ set -e
 PROJECT="$1"
 ACTION="$2"
 
+declare -A port
+port[dmd]='8001'
+port[sma]='8002'
+port[dm1]='8003'
+port[dd]='8004'
 
 function settings() {
     export DJANGO_SETTINGS_MODULE="${PROJECT}.settings"
@@ -107,7 +112,7 @@ function syncmigrate() {
 
 # start runserver
 function startserver() {
-    virt_${PROJECT}/bin/django-admin.py runserver 8000
+    virt_${PROJECT}/bin/django-admin.py runserver ${port[${PROJECT}]}
 }
 
 
