fa54e1ceec8ed6beac461adc7f767c97dedb8d06
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-05-28 16:14:26 +0800

    RDR-106 initial changes

--- dmd/dmd/dmd/views.py
+++ dmd/dmd/dmd/views.py
@@ -35,23 +35,23 @@ def nmd_report(request, working_group):
 
         items['last_follow_up'] = str(d.updated) if d.updated is not None else str(d.created)
 
-        items['muscle_biopsy'] = str(d.muscle_biopsy) if d.muscle_biopsy is not None else ''
+        items['muscle_biopsy'] = yes_no_str(d.muscle_biopsy) if d.muscle_biopsy is not None else 'Unknown'
 
-        items['able_to_walk'] = str(d.motorfunction.walk) if d.motorfunction is not None else ''
-        items['wheelchair_use'] = str(d.motorfunction.wheelchair_use) if d.motorfunction is not None else ''
-        items['able_to_sit'] = str(d.motorfunction.sit) if d.motorfunction is not None else ''
+        items['able_to_walk'] = yes_no_str(d.motorfunction.walk) if d.motorfunction is not None else 'Unknown'
+        items['wheelchair_use'] = yes_no_str(d.motorfunction.wheelchair_use) if d.motorfunction is not None else 'Unknown'
+        items['able_to_sit'] = yes_no_str(d.motorfunction.sit) if d.motorfunction is not None else 'Unknown'
 
-        items['current_steroid_theraphy'] = str(d.steroids.current) if d.steroids is not None else ''
+        items['current_steroid_theraphy'] = yes_no_str(d.steroids.current) if d.steroids is not None else 'Unknown'
 
-        items['scoliosis_surgery'] = str(d.surgery.surgery) if d.surgery is not None else ''
+        items['scoliosis_surgery'] = yes_no_str(d.surgery.surgery) if d.surgery is not None else 'Unknown'
 
-        items['heart'] = str(d.heart.current) if d.heart is not None else ''
-        items['heart_failure'] = str(d.heart.failure) if d.heart is not None else ''
-        items['last_lvef'] = str(d.heart.lvef) if d.heart is not None else ''
+        items['heart'] = yes_no_str(d.heart.current) if d.heart is not None else 'Unknown'
+        items['heart_failure'] = yes_no_str(d.heart.failure) if d.heart is not None else 'Unknown'
+        items['last_lvef'] = yes_no_str(d.heart.lvef) if d.heart is not None else 'Unknown'
 
-        items['non_invasive_ventilation'] = str(d.respiratory.non_invasive_ventilation) if d.respiratory is not None else ''
-        items['invasive_ventilation'] = str(d.respiratory.invasive_ventilation) if d.respiratory is not None else ''
-        items['last_fvc'] = str(d.respiratory.fvc) if d.respiratory is not None else ''
+        items['non_invasive_ventilation'] = yes_no_str(d.respiratory.non_invasive_ventilation) if d.respiratory is not None else 'Unknown'
+        items['invasive_ventilation'] = yes_no_str(d.respiratory.invasive_ventilation) if d.respiratory is not None else 'Unknown'
+        items['last_fvc'] = yes_no_str(d.respiratory.fvc) if d.respiratory is not None else 'Unknown'
 
         molecular_data = MolecularData.objects.filter(patient_id=d.patient.id)
 
@@ -66,26 +66,49 @@ def nmd_report(request, working_group):
             items['deletion_duplication'] = str(variation[0].exon_boundaries_known)
             items['point_mutation'] = str(variation[0].point_mutation_all_exons_sequenced)
         else:
-            items['deletion'] = ''
-            items['duplication'] = ''
-            items['deletion_duplication'] = ''
-            items['point_mutation'] = ''
+            items['deletion'] = 'Unknown'
+            items['duplication'] = 'Unknown'
+            items['deletion_duplication'] = 'Unknown'
+            items['point_mutation'] = 'Unknown'
 
         trials = ClinicalTrials.objects.filter(diagnosis_id=d.id)
-        items['trials'] = str('Y') if trials else str('N')
+        items['trials'] = yes_no_str(trials)
 
         other_regs = OtherRegistries.objects.filter(diagnosis_id=d.id)
-        items['other_registries'] = str('Y') if other_regs else str('N')
+        items['other_registries'] = yes_no_str(other_regs)
 
         family_history = FamilyMember.objects.filter(diagnosis_id=d.id)
-        items['family_history'] = str('Y') if family_history else str('N')
+        items['family_history'] = yes_no_str(family_history)
 
         results.append(items)
 
-    writer.writerow(('Patient ID', 'Mutation Name', 'Deletion', 'Duplication', 'Deletion/Duplication', 'Point Mutation', 'Targeted mutation testing',
-                    'Diagnosis', 'Able To Walk', 'Wheelchair Use', 'Current Steroid Theraphy', 'Scoliosis Surgery',
-                    'Heart', 'Trials', 'Age', 'Last Follow-up', 'Localisation', 'Able To Sit', 'Heart Failure', 'Last LVEF',
-                    'Non Invasive Ventilation', 'Invasive Ventilation', 'Last FVC', 'Muscle Biopsy', 'Other Registries', 'Family History'))
+    writer.writerow((
+        'Patient ID', 
+        'Mutation Name', 
+        'All Exons Tested (Deletion)', 
+        'All Exons Tested (Duplications)', 
+        'Exon Boundaries Known', 
+        'All Exons Sequenced (Point Mutation)', 
+        'All expons tested in Male Relative',
+        'Diagnosis', 
+        'Currently Able To Walk', 
+        'Wheelchair Use', 
+        'Current Steroid Theraphy', 
+        'Scoliosis Surgery',
+        'Current cardiac medication', 
+        'Clinical Trials', 
+        'Date of birth', 
+        'Last updated', 
+        'State/Province, postcode', 
+        'Currently able to sit without support', 
+        'Heart failure/cardiomyopathy', 
+        'LVEF score and date',
+        'Non Invasive Ventilation', 
+        'Invasive Ventilation', 
+        'FVC score and date', 
+        'Previous Muscle Biopsy', 
+        'Other Registries', 
+        'Family History'))
     for r in results:
         writer.writerow((r['patient_id'], '', r['deletion'], r['duplication'], r['deletion_duplication'],
                         r['point_mutation'], '', r['diagnosis'], r['able_to_walk'],
@@ -95,6 +118,9 @@ def nmd_report(request, working_group):
 
     response['Content-Disposition'] = 'attachment; filename=nmdreport_' + working_group + '.csv'
     return response
+
+def yes_no_str(value):
+    return str('Yes') if value else str('No')
 
 # special query for Hugh
 @login_required
