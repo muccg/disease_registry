5b845e669eb0a80e3adc9e71c429929701124e61
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-06-06 15:19:48 +0800

    RDR-103 complete

--- dm1/dm1/dm1/views.py
+++ dm1/dm1/dm1/views.py
@@ -5,12 +5,12 @@ from django.template import RequestContext
 from registry.configuration.models import ConsentForm
 
 def index(request):
-    #consent_form_au = ConsentForm.objects.get(country='AU')
+    consent_form_au = ConsentForm.objects.get(country='AU')
     consent_form_nz = ConsentForm.objects.get(country='NZ')
     
     return render(request, 'dm1/index.html', {
             'media_url': settings.MEDIA_URL,
-            #'consent_form_au': consent_form_au.form,
+            'consent_form_au': consent_form_au.form,
             'consent_form_nz': consent_form_nz.form
         }, 
         context_instance=RequestContext(request))
