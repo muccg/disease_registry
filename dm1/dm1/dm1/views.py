d5ef15d60a504769f0f47c90b06b9d4c5161eeae
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-06-07 10:45:03 +0800

    Consent form can be module specific

--- dm1/dm1/dm1/views.py
+++ dm1/dm1/dm1/views.py
@@ -5,8 +5,10 @@ from django.template import RequestContext
 from registry.configuration.models import ConsentForm
 
 def index(request):
-    consent_form_au = ConsentForm.objects.get(country='AU')
-    consent_form_nz = ConsentForm.objects.get(country='NZ')
+    consent_form_module = ConsentForm.objects.filter(module__code='DM1')
+    
+    consent_form_au = consent_form_module.get(country='AU')
+    consent_form_nz = consent_form_module.get(country='NZ')
     
     return render(request, 'dm1/index.html', {
             'media_url': settings.MEDIA_URL,
