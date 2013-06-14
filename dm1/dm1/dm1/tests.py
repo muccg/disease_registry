4ebd3213aba453910ba9e2cf16fb3e55ac675108
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-06-14 21:23:21 +0800

    Sample Selenium test for DM1 + fixture fix

--- dm1/dm1/dm1/tests.py
+++ dm1/dm1/dm1/tests.py
@@ -7,17 +7,34 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    def test_molecular_data_page(self):
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+                
+        browser = webdriver.Firefox()
+        
+        browser.get("http://192.168.251.178:8080/admin/")
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        usernameInput = browser.find_element_by_name("username")
+        usernameInput.send_keys("admin")
+        passwordInput = browser.find_element_by_name("password")
+        passwordInput.send_keys("admin")
 
->>> 1 + 1 == 2
-True
-"""}
+        passwordInput.submit()
 
+        self.assertEqual('Site administration | Django site admin', browser.title)
+
+        browser.get("http://192.168.251.178:8080/admin/genetic/moleculardata/")
+
+        self.assertIsNotNone(browser.find_element_by_xpath("//*[contains(.,'Select molecular data to change')]"))
+
+        browser.get("http://192.168.251.178:8080/admin/logout")
+
+        self.assertEqual('Logged out | Django site admin', browser.title)
+
+        browser.quit()
+        display.stop()
