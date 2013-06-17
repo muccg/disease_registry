0bec7f2bf2edd4734fc047de5ac49041a051b11d
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-06-17 13:27:44 +0800

    DM1 test refactor

--- dm1/dm1/dm1/tests.py
+++ dm1/dm1/dm1/tests.py
@@ -1,40 +1,53 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+import urllib2
 
 from django.test import TestCase
+from django.test.client import Client
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
 class SimpleTest(TestCase):
-    def test_molecular_data_page(self):
-        display = Display(visible=0, size=(800, 600))
-        display.start()
-                
-        browser = webdriver.Firefox()
+    USERNAME = "curator"
+    PASSWORD = "curator"
+    HOST = "http://192.168.251.178:8080"
+    
+    def setUp(self):
+        self.DISPLAY = Display(visible=0, size=(800, 600))
+        self.DISPLAY.start()
+        self.DRIVER = webdriver.Firefox()
+        self.login()
         
-        browser.get("http://192.168.251.178:8080/admin/")
+    def tearDown(self):
+        self.logout()
+        self.DRIVER.quit()
+        self.DISPLAY.stop()
+    
+    def test_response_all_pages(self):
+        links = self.DRIVER.find_elements_by_tag_name("a")
+        
+        for link in links:
+            attr = link.get_attribute("href")
+            if "logout" not in attr:
+                response_code = self.get_response_code(attr)
+                print "%s -> %s" % (attr, response_code)
+                self.assertEqual(int(response_code), 200)
 
-        usernameInput = browser.find_element_by_name("username")
-        usernameInput.send_keys("admin")
-        passwordInput = browser.find_element_by_name("password")
-        passwordInput.send_keys("admin")
+    def test_molecular_data_page(self):
+        self.DRIVER.get("%s/admin/genetic/moleculardata/" % self.HOST)
+        self.assertIsNotNone(self.DRIVER.find_element_by_xpath("//*[contains(.,'Select molecular data to change')]"))
 
+    def get_response_code(self, url):
+        response = urllib2.urlopen(url)
+        return response.getcode()
+    
+    def login(self):
+        self.DRIVER.get("%s/admin/" % self.HOST)
+        usernameInput = self.DRIVER.find_element_by_name("username")
+        usernameInput.send_keys(self.USERNAME)
+        passwordInput = self.DRIVER.find_element_by_name("password")
+        passwordInput.send_keys(self.PASSWORD)
         passwordInput.submit()
-
-        self.assertEqual('Site administration | Django site admin', browser.title)
-
-        browser.get("http://192.168.251.178:8080/admin/genetic/moleculardata/")
-
-        self.assertIsNotNone(browser.find_element_by_xpath("//*[contains(.,'Select molecular data to change')]"))
-
-        browser.get("http://192.168.251.178:8080/admin/logout")
-
-        self.assertEqual('Logged out | Django site admin', browser.title)
-
-        browser.quit()
-        display.stop()
+    
+    def logout(self):
+        self.DRIVER.get("%s/admin/logout" % self.HOST)
+        self.assertEqual('Logged out | Django site admin', self.DRIVER.title)
