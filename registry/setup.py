2ae72ff60d05da78863be58f551c04595b206210
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-07-05 11:48:04 +0800

    Add lettuce features folder to rpm build for all modules

--- registry/setup.py
+++ registry/setup.py
@@ -7,7 +7,7 @@ start_dir = os.getcwd()
 for package in ('common', 'patients', 'genetic', 'groups', 'humangenome'):
     data_files['registry.' + package] = []
     os.chdir(os.path.join('registry', package))
-    for data_dir in ('templates', 'static', 'migrations', 'fixtures', 'templatetags'):
+    for data_dir in ('templates', 'static', 'migrations', 'fixtures', 'templatetags', 'features'):
 	   data_files['registry.' + package].extend(
 	       [os.path.join(subdir,f) for (subdir, dirs, files) in os.walk(data_dir) for f in files])
     os.chdir(start_dir)
@@ -18,7 +18,7 @@ for package in ('common', 'patients', 'genetic', 'groups', 'humangenome'):
 for package in ('common', 'patients', 'genetic', 'groups', 'humangenome'):
     data_files['registry.' + package] = []
     os.chdir(os.path.join('registry', package))
-    for data_dir in ('templates', 'static', 'migrations', 'fixtures', 'templatetags'):
+    for data_dir in ('templates', 'static', 'migrations', 'fixtures', 'templatetags', 'features'):
        data_files['registry.' + package].extend(
            [os.path.join(subdir,f) for (subdir, dirs, files) in os.walk(data_dir) for f in files])
     os.chdir('../..')
