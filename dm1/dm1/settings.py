629d78cc1158baba8f0cab7586bc3249663b7b15
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-07-09 16:19:25 +0800

    RDR-137 DM1 DeprecationWarning: You have no filters defined on the mail_admins logging handler

--- dm1/dm1/settings.py
+++ dm1/dm1/settings.py
@@ -191,6 +191,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': [],
             'class': 'django.utils.log.AdminEmailHandler',
             'formatter':'verbose',
             'include_html':True
