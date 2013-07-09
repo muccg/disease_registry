b76968719ad4a9ac5628683c57124cb02bae95e9
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-07-09 16:41:54 +0800

    RDR-137 DD, FSHD, SMA django.conf.urls.defaults is deprecated

--- fshd/fshd/fshd/urls.py
+++ fshd/fshd/fshd/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns
-from django.conf.urls.defaults import include
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = patterns('',
