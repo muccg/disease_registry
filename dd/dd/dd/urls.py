ce0c49131292ad84205a80153b4980f06a418adf
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-07-09 16:41:54 +0800

    RDR-137 DD, FSHD, SMA django.conf.urls.defaults is deprecated

--- dd/dd/dd/urls.py
+++ dd/dd/dd/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns
 
 urlpatterns = patterns('',
     (r"^$", "django.views.generic.simple.direct_to_template",{"template": "dd/index.html"})
