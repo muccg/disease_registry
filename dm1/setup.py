ed8ed8713f81cb5e8170d88e76949948fe5bfa52
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-07-25 13:07:17 +0800

    Version to 1.5.0

--- dm1/setup.py
+++ dm1/setup.py
@@ -27,7 +27,7 @@ for package in ('common', 'patients', 'genetic', 'groups', 'humangenome'):
 os.chdir('../dm1')
 
 setup(name='django-dm1registry',
-    version='1.4.0',
+    version='1.5.0',
     description='Django Disease Registry - DM1',
     long_description='Django Disease registry for DM1',
     author='Centre for Comparative Genomics',
