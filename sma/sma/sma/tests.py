00fe0fda4af03a026c48ef3727cde51231d17e07
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-08-08 16:01:51 +0800

    SMA call_command modification in order ot run all features

--- sma/sma/sma/tests.py
+++ sma/sma/sma/tests.py
@@ -2,11 +2,6 @@ from django.test import TestCase
 
 from django.core.management import call_command
 
-class LettuceLoginTests(TestCase):
+class LettuceTests(TestCase):
     def test_patient(self):
-        call_command('harvest', 'sma/sma/features/login.feature')
-        call_command('harvest', 'sma/sma/features/report_links.feature')
-
-class LettuceReportLinksTests(TestCase):
-    def test_report_links(self):
-        call_command('harvest', 'sma/sma/features/report_links.feature')
+        call_command('harvest', 'sma/sma/features/')
