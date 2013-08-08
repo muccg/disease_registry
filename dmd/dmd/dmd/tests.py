fdae912b426045630aaf609ed4e1a78875645c50
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-08-08 16:05:12 +0800

    DMD call_command modification in order to run all features

--- dmd/dmd/dmd/tests.py
+++ dmd/dmd/dmd/tests.py
@@ -32,17 +32,9 @@ from django.core.management import call_command
 #     '--cover-package=dmd',
 # ]
 
-class LettuceLoginTests(TestCase):
-    def test_login(self):
-        call_command('harvest', 'dmd/dmd/features/report_links.feature')
-
-class LettucePatientTests(TestCase):
-    def test_patient(self):
-        call_command('harvest', 'dmd/dmd/features/patient.feature')
-
-class LettuceReportLinksTests(TestCase):
-    def test_report_links(self):
-        call_command('harvest', 'dmd/dmd/features/report_links.feature')
+class LettuceTests(TestCase):
+    def test_run(self):
+        call_command('harvest', 'dmd/dmd/features/')
 
 class PatchedNoseTestSuiteRunner(NoseTestSuiteRunner):
     """This class required by django_nose to correctly load all models, not sure why. See,
