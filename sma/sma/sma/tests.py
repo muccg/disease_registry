0502d685da9d864d38846b13ba3d0c851a79c09a
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-08-19 14:39:47 +0800

    SMA remove lettuce from tests.py and terrain adjustments

--- sma/sma/sma/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-from django.core.management import call_command
-
-class LettuceTests(TestCase):
-    def test_run(self):
-        call_command('harvest', 'sma/sma/features/')
