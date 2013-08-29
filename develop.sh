bd91493bb9a2577c359cc5a1393111e82067ca94
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-08-29 12:18:40 +0800

    Change owner of /var/www before lettuce tests

--- develop.sh
+++ develop.sh
@@ -85,6 +85,7 @@ function ci_staging() {
 # staging seleinium test
 function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:'dbus-uuidgen --ensure'
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'chown apache:apache /var/www'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'dmd run_lettuce --with-xunit --xunit-file\=/tmp/tests.xml || true'
     ccg ${AWS_STAGING_INSTANCE} getfile:/tmp/tests.xml,./
     #ccg ${AWS_STAGING_INSTANCE} dsudo:'dmd harvest dmd/dmd/features/*.feature'
