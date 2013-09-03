20210a6bb069700720c2126dde905a8742839494
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-09-03 12:18:07 +0800

    DM1, FSHD add configuration to setup.py

--- fshd/setup.py
+++ fshd/setup.py
@@ -17,7 +17,7 @@ for package in ('fshd', 'fshd_questionnaire'):
 # Include common disease registry modules from registry sibling directory
 # This isn't what we want to be doing because we need to do this in the other
 # registry apps too. Instead there should be a single setup.py for all the registries (IMO)
-for package in ('common', 'patients', 'genetic', 'groups', 'humangenome'):
+for package in ('common', 'patients', 'genetic', 'groups', 'humangenome', 'configuration'):
     data_files['registry.' + package] = []
     os.chdir(os.path.join('registry', package))
     for data_dir in ('templates', 'static', 'migrations', 'fixtures', 'templatetags', 'features', 'management'):
