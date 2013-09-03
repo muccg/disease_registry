a153bff4efaa8f290f7e2ee6a0836cc061e8d3be
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-09-03 14:43:42 +0800

    FSHD static html template problem

--- fshd/fshd/fshd/urls.py
+++ fshd/fshd/fshd/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns
 from django.conf.urls import include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-                       (r"^$", "django.views.generic.simple.direct_to_template",
-                        {"template": "fshd/index.html"}))
+    (r"^$", TemplateView.as_view(template_name='fshd/index.html')),
+)
 
 urlpatterns += patterns('',
-                        (r'^admin/', include(admin.site.urls), {}),
-                        )
+    (r'^admin/', include(admin.site.urls), {}),
+)
