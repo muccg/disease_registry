214b22551603812f69db61dd9299d3bebc577c42
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-09-03 12:43:46 +0800

    DM1 remove dump to tests.xml file in lettuce hook

--- dm1/dm1/dm1/features/terrain.py
+++ dm1/dm1/dm1/features/terrain.py
@@ -20,8 +20,5 @@ def set_browser():
 
 @after.all
 def clean_after_tests(result):
-    tests_xml = open("tests.xml", "w")
-    tests_xml.write("<testsuite errors='%d' tests='%d'></testsuite>" % (result.scenarios_ran - result.scenarios_passed, result.scenarios_ran) )
-    tests_xml.close()
     if world.browser != None: world.browser.quit()
     if display: display.stop()
