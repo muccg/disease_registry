64da2c1f19fe57174e96c379a5d15e521d85c4f0
Lee Render <lrender@ccg.murdoch.edu.au>
2013-09-27 14:32:41 +0800

    RDR-218 dd terrain file resets cookies before each scenario to ensure login page presented as expected in tests.

--- dd/dd/dd/features/terrain.py
+++ dd/dd/dd/features/terrain.py
@@ -34,3 +34,7 @@ def clean_after_tests(result):
     if display: display.stop()
 
 
+@before.each_scenario
+def delete_cookies(scenario):
+    # delete all cookies so when we browse to a url at the start we have to log in
+    world.browser.delete_all_cookies()
