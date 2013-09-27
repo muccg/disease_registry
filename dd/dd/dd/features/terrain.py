20887c9ce94ae79e480b8a2c9e632e5ade67e553
Lee Render <lrender@ccg.murdoch.edu.au>
2013-09-27 14:31:43 +0800

    RDR-218 Updared terrain file for dd to allow injected site url

--- dd/dd/dd/features/terrain.py
+++ dd/dd/dd/features/terrain.py
@@ -20,7 +20,17 @@ def set_browser():
     if display: display.start()
     world.browser = webdriver.Firefox()
 
+@before.all
+def set_site_url():
+    world.site_url = steps.get_site_url(default_url="http://localhost:8002")
+
+@before.all
+def set_wait_seconds():
+    world.wait_seconds = 3
+
 @after.all
 def clean_after_tests(result):
     world.browser.quit()
     if display: display.stop()
+
+
