bceb7b59e8995d175d14ef56e45160b5242c06aa
Lee Render <lrender@ccg.murdoch.edu.au>
2013-10-02 10:14:50 +0800

    RDR-218 Removed site_url files via ccg remote shell command

--- develop.sh
+++ develop.sh
@@ -96,6 +96,7 @@ function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:'echo http://localhost/dmd > /tmp/dmd_site_url'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'dmd run_lettuce --app-name dmd --with-xunit --xunit-file\=/tmp/tests-dmd.xml || true'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum remove dmd -y'
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'rm /tmp/dmd_site_url'
     
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum install sma -y'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'killall httpd || true'
@@ -103,6 +104,7 @@ function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:'echo http://localhost/sma > /tmp/sma_site_url'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'sma run_lettuce --app-name sma --with-xunit --xunit-file\=/tmp/tests-sma.xml || true'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum remove sma -y'
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'rm /tmp/sma_site_url'
     
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum install dm1 -y'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'killall httpd || true'
@@ -110,13 +112,15 @@ function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:'echo http://localhost/dm1 > /tmp/dm1_site_url'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'dm1 run_lettuce --app-name dm1 --with-xunit --xunit-file\=/tmp/tests-dm1.xml || true'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum remove dm1 -y'
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'rm /tmp/dm1_site_url'
     
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum install dd -y'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'killall httpd || true'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'service httpd start'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'echo http://localhost/dd > /tmp/dd_site_url'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'registrydd run_lettuce --app-name dd --with-xunit --xunit-file\=/tmp/tests-dd.xml || true'
-    
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'rm /tmp/dd_site_url'
+
     ccg ${AWS_STAGING_INSTANCE} getfile:/tmp/tests-dmd.xml,./
     ccg ${AWS_STAGING_INSTANCE} getfile:/tmp/tests-sma.xml,./
     ccg ${AWS_STAGING_INSTANCE} getfile:/tmp/tests-dm1.xml,./
