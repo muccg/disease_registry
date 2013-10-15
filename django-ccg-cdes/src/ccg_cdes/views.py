8eb71ca166e2ee85719c428e69c8aeb269b7bd65
Lee Render <lrender@ccg.murdoch.edu.au>
2013-10-15 22:11:24 +0800

    Saving works

--- django-ccg-cdes/src/ccg_cdes/views.py
+++ django-ccg-cdes/src/ccg_cdes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render_to_response
-from django.forms import Form
+from django.forms import BaseForm
 from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.conf import settings
 from models import CDEValue, CommonDataElement
 from django.core.context_processors import csrf
+import logging
+
+logger = logging.getLogger("dmd")
 
 #todo generalise this to work across all owner types
 
@@ -16,44 +19,37 @@ def patient_cdes(request, patient_id):
     except owner_model.DoesNotExist:
         raise Http404("Patient does not exist")
 
-    class CDEForm(Form):
-        pass
-
     cde_map = {}
     field_map = {}
 
     for cde in CommonDataElement.objects.all().filter(owner='Patient'):
-        print "got cde %s" % cde
+        logger.debug("got cde %s" % cde)
         cde_field = cde.create_field()
-        field_name = "cde_" + cde.name
+        field_name = cde.code
         cde_map[field_name] = cde
         field_map[field_name] = cde_field
-        setattr(CDEForm, field_name, cde_field)
 
-    def get_data_for_instance(self, owner_instance):
+    def get_data_for_instance(owner_instance):
         values = {}
         owner_class_name = owner_instance.__class__.__name__
         cdes = CDEValue.objects.all().filter(owner_class=owner_class_name, owner_id=owner_instance.pk)
-        for field in self.fields:
-            cde_element = field_map[field]  # The CDE Element corresponding to the field
-            cde_value = cdes.filter(element=cde_element).get()
-            if cde_value:
-                values[cde_value.code] = cde_value.value
+
+        for field in field_map:
+            cde_element = cde_map[field]  # The CDE Element corresponding to the field
+            try:
+                cde_value = cdes.filter(element=cde_element).get()
+                values[cde_element.code] = cde_value.code
+            except CDEValue.DoesNotExist:
+                # may happen if we unset
+                pass
+
+        print "values for post = %s" % values
+        return values
 
 
 
 
-
-
-
-    real_form_dict = {}
-
-    for k, v  in CDEForm.__dict__.items():
-        real_form_dict[k] = v
-
-
-
-    form_class = type('CDEForm', (Form,), real_form_dict)
+    form_class = type('CDEForm', (BaseForm,), {"base_fields": field_map})
 
     def get_value_from_code(cde_field, code):
         for (c,v) in cde_field.choices:
@@ -62,30 +58,40 @@ def patient_cdes(request, patient_id):
 
     if request.method == "POST":
         form = form_class(request.POST)
-
+        print "in post"
         if form.is_valid():
+
+            logger.debug("form is valid")
             # process data
             # update/create the associated CDEValues
             for field in form.fields:
+                logger.debug("updating data from field %s" % field)
                 code = form.cleaned_data[field]
-                if code == "UNSET":
-                    continue
                 element = cde_map[field]
                 cde_field = field_map[field]
                 cde_value, created = CDEValue.objects.get_or_create(owner_class="Patient", owner_id=patient_id, element=element)
+                if created:
+                    logger.debug("created CDEValue for field")
+                else:
+                    logger.debug("updating existing data for field")
+
+                if code == "UNSET":
+                    # delete the value
+                    cde_value.delete()
+                    continue
 
                 # store code and the value because later will support "Other please specify" ( which has a code but allows free value )
                 cde_value.code = code
                 value = get_value_from_code(cde_field, code)
                 cde_value.value = value
                 cde_value.save()
+                logger.debug("CDEValue saved: %s" % cde_value)
 
             return HttpResponseRedirect('/cdes/patient/%s' % patient_id)
     else:
-        form = form_class()
+        form = form_class(get_data_for_instance(owner_instance))
 
-    context = {'form': form,
-                                        'owner':  'patient',
+    context = {'form': form, 'owner':  'patient',
                                         'owner_id': patient_id,
                                         'name': owner_instance.given_names + " " + owner_instance.family_name,
                 }
