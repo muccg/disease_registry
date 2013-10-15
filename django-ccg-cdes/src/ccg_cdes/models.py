cb8b90fd1e031d7b95e3857d83e49f41151ab7c7
Lee Render <lrender@ccg.murdoch.edu.au>
2013-10-15 20:49:29 +0800

    Changes to CDE models to allow config from settings. Fixed code /value on CDE value for consistency

--- django-ccg-cdes/src/ccg_cdes/models.py
+++ django-ccg-cdes/src/ccg_cdes/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
+
+def get_owner_choices():
+    """
+    Get choices for CDE owner drop down.
+    Used to get the list of classes which CDEs can be attached to.
+    UNUSED means this CDE will not be used to construct any forms in the registry.
+
+    """
+    from django.conf import settings
+    choices = [('UNUSED', 'UNUSED')]
+    for display_name, owner_model_func in settings.CDE_MODEL_MAP.items():
+        owner_class_name = owner_model_func().__name__
+        choices.append((owner_class_name, display_name))
+    return choices
+
 class CDEPermittedValueGroup(models.Model):
     code = models.CharField(max_length=30, primary_key=True)
     name = models.CharField(max_length=50)
+
     def __unicode__(self):
         return "PVG %s:%s" % (self.code,self.name)
 
@@ -11,6 +27,7 @@ class CDEPermittedValue(models.Model):
     value = models.CharField(max_length=50)
     desc = models.CharField(max_length=50)
     pv_group = models.ForeignKey(CDEPermittedValueGroup,related_name='permitted_value_set')
+
     def __unicode__(self):
         return "PV %s:%s of %s" % (self.code,self.value,self.pv_group)
 
@@ -33,23 +50,21 @@ class CommonDataElement(models.Model):
     disease = models.CharField(max_length=50)
     pv_group = models.ForeignKey(CDEPermittedValueGroup)
 
-    OWNER_CHOICES = (("Patient", "Patient"),
-                     ("D", "Diagnosis"),
-                     ("U", "Unused"))
+    OWNER_CHOICES = get_owner_choices()
 
-    owner = models.CharField(max_length=1, choices=OWNER_CHOICES)
+    owner = models.CharField(max_length=50, choices=OWNER_CHOICES, default="UNUSED")
 
     def __unicode__(self):
         return "CDE %s:%s" % (self.code, self.name)
 
-    def create_field(self, owner_model_class):
+    def create_field(self):
         from django import forms
 
-        choices = []
+        choices = [('UNSET', '---')]  # default unselected option
         has_specify = False
         field_name = self.name
         for permitted_value in self.pv_group.permitted_value_set.all():
-            choice_tuple = (permitted_value.value, permitted_value.desc)
+            choice_tuple = (permitted_value.code, permitted_value.value)
             print "added choice %s" % permitted_value.value
             choices.append(choice_tuple)
 
@@ -59,17 +74,20 @@ class CommonDataElement(models.Model):
         else:
             # choice field - ignore the "other - please specify for now
             # complication
+            # add default unset option
+
+
             print "returning choices %s for field %s" % (choices, field_name)
 
-            return forms.ChoiceField(choices=choices,label=field_name)
+            return forms.ChoiceField(choices=choices,label=field_name, initial='UNSET', required=True)
 
 
 class CDEValue(models.Model):
     element = models.ForeignKey(CommonDataElement)
     owner_id = models.IntegerField()
-    owner_class = models.CharField(max_length=50) # the class name this cde value belongs to
-    data = models.TextField() # the value selected or entered by the user
-    code = models.CharField(max_length=50) # the code for this choice
+    owner_class = models.CharField(max_length=50)   # the class name this cde value belongs to
+    code = models.CharField(max_length=50)          # the code for this choice   ( e.g. PV001 etc )
+    value = models.TextField()                      #the value selected or entered by the user
 
 
 
