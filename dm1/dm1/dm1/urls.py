92f116e97a00c9031eed8425abb7c8f7266444c6
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-10-29 16:36:13 +0800

    Remove unnecessary link to urls.py

--- dm1/dm1/dm1/urls.py
+++ dm1/dm1/dm1/urls.py
@@ -4,7 +4,3 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        (r"^$", 'dm1.dm1.views.index'))
-
-urlpatterns += patterns('',
-                        (r'^admin/', include(admin.site.urls), {}),
-                        )
