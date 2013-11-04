a2321768de858f2d21b29f8c5e067c19d89d352d
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-11-04 16:33:51 +0800

    Middleware for logging response time

---
+++ registry/registry/common/middleware.py
@@ -0,0 +1,30 @@
+import time
+import logging
+from django.db import connection
+from django.utils.encoding import smart_str
+
+logger = logging.getLogger(__name__)
+
+class TimeLogMiddleware(object):
+
+    def process_request(self, request):
+        request._start = time.time()
+
+    def process_response(self, request, response):
+        sqltime = 0.0
+
+        for q in connection.queries:
+            sqltime += float(getattr(q, 'time', 0.0))
+
+        if hasattr(request, '_start'):
+            d = {
+                'method': request.method,
+                'time': time.time() - request._start,
+                'code': response.status_code,
+                'url': smart_str(request.path_info),
+                'sql': len(connection.queries),
+                'sqltime': sqltime,
+            }
+            msg = '%(method)s "%(url)s" (%(code)s) %(time).2f (%(sql)dq, %(sqltime).4f)' % d
+            logger.info(msg)
+        return response
