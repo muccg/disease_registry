35af85b09af339c2896ccc8b16eb8d35f56748d0
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-05 14:18:20 +0800

    removed django-ccg-cdes

--- django-ccg-cdes/src/ccg_cdes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from models import *
-admin.site.register(CDEPermittedValue)
-admin.site.register(CDEPermittedValueGroup)
-admin.site.register(CommonDataElement)
-admin.site.register(CDEValue)
