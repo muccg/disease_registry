0e1a9c4adb005b5aac4ace6a72ad91493b454e5f
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-12-02 13:11:24 +0800

    RDR-258 login required for new report

--- registry/registry/common/genetic_report.py
+++ registry/registry/common/genetic_report.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.views.generic.base import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from django.db.models import Q
 
 from registry.genetic.models import Variation
 
 class GeneticReport(View):
+    
+    @method_decorator(login_required)
     def get(self, request):
         varations = Variation.objects.get_containing('protein_variation', ['X', '*'])
         
