1a7be4a14fe902017a8d8343897bc7dd469a00f4
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-12-02 12:19:09 +0800

    RDR-258 custom manager for Variation model

--- registry/registry/common/genetic_report.py
+++ registry/registry/common/genetic_report.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic.base import View
 
+from django.db.models import Q
+
+from registry.genetic.models import Variation
+
 class GeneticReport(View):
     def get(self, request):
-        return HttpResponse('result')
+        varations = Variation.objects.get_containing('protein_variation', ['X', '*'])
+        
+        return HttpResponse(varations)
