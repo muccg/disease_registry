1bcb64484d16f0ff4d7bc80e99bb0a2a82d72ada
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-12-03 21:54:28 +0800

    RDR-258 stratify results by age groups

--- registry/registry/common/genetic_report.py
+++ registry/registry/common/genetic_report.py
@@ -16,13 +16,13 @@ from dmd.dmd.models import Diagnosis, MotorFunction, Steroids, Respiratory, Clin
 class GeneticReport(View):
     
     age_range = [
-        ['Up to 3 years', ('02.09.2009', '01.01.2100')],
-        ['Aged 4 to 6', ('02.09.2006', '01.09.2009')],
-        ['Aged 7 to 9', ('02.09.2003', '01.09.2006')],
-        ['Aged 10 to 12', ('02.09.2000', '01.09.2003')],
-        ['Aged 13 to 15', ('02.09.1997', '01.09.2000')],
-        ['Aged 16 to 18', ('02.09.1994', '01.09.1997')],
-        ['Aged 19 and over', ('01.01.1900', '01.09.1994')]
+        ['Up to 3 years', '02.09.2009', '01.01.2100'],
+        ['Aged 4 to 6', '02.09.2006', '01.09.2009'],
+        ['Aged 7 to 9', '02.09.2003', '01.09.2006'],
+        ['Aged 10 to 12', '02.09.2000', '01.09.2003'],
+        ['Aged 13 to 15', '02.09.1997', '01.09.2000'],
+        ['Aged 16 to 18', '02.09.1994', '01.09.1997'],
+        ['Aged 19 and over', '01.01.1900', '01.09.1994']
     ]
     
     spec = [
@@ -44,14 +44,16 @@ class GeneticReport(View):
         response = HttpResponse(mimetype="text/csv")
         writer = csv.writer(response)
         
-        variations = Variation.objects.get_containing('protein_variation', ['X', '*'])
-        
-        report = [self.get_results(v) for v in variations]
-        
-        writer.writerow([s[0] for s in self.spec])
-        
-        for r in report:
-            writer.writerow([r[s[1]] for s in self.spec])
+        for age in self.age_range:
+            variations = Variation.objects.get_containing('protein_variation', ['X', '*'])
+            variations = variations.filter(molecular_data__patient__date_of_birth__range=(self.str_to_date(age[1]), self.str_to_date(age[2])))
+            if variations:
+                report = [self.get_results(v) for v in variations]
+                writer.writerow([age[0]])    
+                writer.writerow([s[0] for s in self.spec])
+                for r in report:
+                    writer.writerow([r[s[1]] for s in self.spec])
+                writer.writerow([''])
 
         response['Content-Disposition'] = 'attachment; filename=report.csv'
         return response
