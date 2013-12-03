52b2598aeab1639fab9fd4d06f6265f1d0e97594
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-12-03 16:30:58 +0800

    RDR-258 report redone

--- registry/registry/common/genetic_report.py
+++ registry/registry/common/genetic_report.py
@@ -1,3 +1,5 @@
+import csv
+
 from datetime import datetime
 
 from django.http import HttpResponse
@@ -9,7 +11,7 @@ from django.db.models import Q
 
 from registry.genetic.models import Variation
 from registry.patients.models import Patient
-from dmd.dmd.models import Diagnosis, MotorFunction
+from dmd.dmd.models import Diagnosis, MotorFunction, Steroids, Respiratory, ClinicalTrials
 
 class GeneticReport(View):
     
@@ -23,36 +25,94 @@ class GeneticReport(View):
         ('01.01.1900', '01.09.1994')
     ]
     
+    spec = [
+        ['Protein Variation', 'proteinvariation'],
+        ['Gene', 'gene'],
+        ['Exon', 'exon'],
+        ['DNA Variation', 'dna_variation'],
+        ['All exons tested deletions', 'exons_deletions'],
+        ['All exons tested duplications', 'exons_duplications'],
+        ['Currently able to walk', 'walk'],
+        ['Current steroids use', 'steroids'],
+        ['Non-invasive ventilation', 'non_invasive_ventilation'],
+        ['Invasive ventilation','invasive_ventilation'],
+        ['Clinical Trials', 'trials']
+    ]
+    
     @method_decorator(login_required)
     def get(self, request):
-        varations = Variation.objects.get_containing('protein_variation', ['X', '*'])
-
-        for ages in self.age_range:
-            patients_from_variation = [v.molecular_data.patient for v in varations]
-            patients = Patient.objects.filter(id__in=[patient.id for patient in patients_from_variation])
-            patients = patients.filter(date_of_birth__range=[self.str_to_date(ages[0]), self.str_to_date(ages[1])])
-
-            diagnosis = Diagnosis.objects.filter(patient__in=patients)
-
-            print '\n---%s - %s --- total: %s' % (ages[0], ages[1], patients.count())
-            print 'ambulant         %s' % diagnosis.filter(motorfunction__walk=True).count()
-            print 'nonambulant      %s' % diagnosis.filter(motorfunction__walk=False).count()
-            print 'ambulantunknown  %s\n' % diagnosis.filter(motorfunction__walk__isnull=True).count()
-
-            print 'on_steroids      %s' % diagnosis.filter(steroids__current=True).count()
-            print 'not_on_steroids  %s' % diagnosis.filter(steroids__current=False).filter(steroids__previous=0).count()
-            print 'unknown          %s\n' % diagnosis.filter(steroids__current__isnull=True).count()
-
-            #To be confirmed with Leanne
-            ventilation_query = Q(respiratory__invasive_ventilation='Y') | Q(respiratory__non_invasive_ventilation='Y')
-            print 'ventialation_yes %s' % diagnosis.filter(ventilation_query).count()
-            print 'ventialation_no  %s' % diagnosis.filter(~ventilation_query).count()
-            print 'unknown          %s\n' % diagnosis.filter(respiratory__invasive_ventilation__isnull=True).count()
-            
-            print 'yes_clinical_trails  %s\n' % diagnosis.filter(clinicaltrials__isnull=False).count()
-            print 'unknown_clinical_trails  %s\n' % diagnosis.filter(clinicaltrials__isnull=True).count()
+        response = HttpResponse(mimetype="text/csv")
+        writer = csv.writer(response)
         
-        return HttpResponse('Done')
+        variations = Variation.objects.get_containing('protein_variation', ['X', '*'])
+        
+        report = [self.get_results(v) for v in variations]
+        
+        writer.writerow([s[0] for s in self.spec])
+        
+        for r in report:
+            writer.writerow([r[s[1]] for s in self.spec])
+
+        response['Content-Disposition'] = 'attachment; filename=report.csv'
+        return response
+
+    def get_results(self, variation):
+        results = {}
+        
+        results['proteinvariation'] = variation.protein_variation
+        results['gene'] = variation.gene
+        results['exon'] = variation.exon
+        results['dna_variation'] = variation.dna_variation
+        results['exons_deletions'] = self.yes_no_unknown(variation.deletion_all_exons_tested)
+        results['exons_duplications'] = self.yes_no_unknown(variation.duplication_all_exons_tested)
+        
+        diagnosis = Diagnosis.objects.get(patient=variation.molecular_data.patient)
+        
+        try:
+            motor = MotorFunction.objects.get(diagnosis=diagnosis)
+            results['walk'] = self.yes_no_unknown(motor.walk)
+        except MotorFunction.DoesNotExist:
+            results['walk'] = 'No/Unknown'
+            
+        try:
+            steroids = Steroids.objects.get(diagnosis=diagnosis)
+            results['steroids'] = self.yes_no_unknown(steroids.current)
+        except Steroids.DoesNotExist:
+            results['steroids'] = 'Unknown'
+            
+        try:
+            respiratory = Respiratory.objects.get(diagnosis=diagnosis)
+            results['non_invasive_ventilation'] = self.yes_part_no_unknown(respiratory.non_invasive_ventilation)
+            results['invasive_ventilation'] = self.yes_part_no_unknown(respiratory.invasive_ventilation)
+        except Respiratory.DoesNotExist:
+            results['non_invasive_ventilation'] = 'Unknown'
+            results['invasive_ventilation'] = 'Unknown'
+            
+        try:
+            trials = ClinicalTrials.objects.get(diagnosis=diagnosis)
+            results['trials'] = 'Yes'
+        except ClinicalTrials.DoesNotExist:
+            results['trials'] = 'No'
+            
+        return results
+
+    def yes_part_no_unknown(self, value):
+        if value is None:
+            return 'Unknown'
+        if value == 'Y':
+            return 'Yes'
+        if value == 'PT':
+            return 'Yes - part time'
+        if value == 'N':
+            return 'No'
+
+    def yes_no_unknown(self, value):
+        if value is None:
+            return 'Unknown'
+        if value:
+            return 'Yes'
+        elif not value:
+            return 'No'
 
     def str_to_date(self, value):
         return datetime.strptime(value , '%d.%m.%Y').date()
