6efdd38cb23486d2e4ba93196ce06a0d57d9e3f1
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-01-29 09:54:04 +0900

    Version number templatetag parametrized with app name

--- registry/registry/configuration/templatetags/version_number.py
+++ registry/registry/configuration/templatetags/version_number.py
@@ -1,9 +1,9 @@
 from django import template
-from ... import VERSION
 
 register = template.Library()
 
 # settings value
 @register.simple_tag
-def version_number():
-    return VERSION
+def version_number(app_name):
+    module = __import__(app_name)
+    return module.VERSION
