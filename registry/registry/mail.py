3a596f632a58f0729e8c47c43d802c1df50e27c8
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2015-02-23 10:45:58 +0800

    RDR-693 Error message on saving data

--- registry/registry/mail.py
+++ registry/registry/mail.py
@@ -14,21 +14,23 @@ def sendNewPatientEmail(recipients, from_email=None):
     if not from_email:
         from_email = default_return_email()
 
-    template = getNewPatientEmailTemplate()
-    groups = template.groups.all()
-    to_email = recipients.filter(user__groups__in=groups).distinct().values_list("user__email", flat=True)
-    subject = '%s: new patient registered' % settings.INSTALL_NAME.upper()
-    body = template.body
-
-    try:
-        mass_email = []
-        for email in to_email:
-            mass_email.append( (subject, body, from_email, [email]) )
-
-        send_mass_mail(mass_email)
-    except Exception, e:
-        print 'Error sending mail to user: ',to_email , ':', str(e)
+    templates = getNewPatientEmailTemplate()
+    
+    for template in templates:
+        groups = template.groups.all()
+        to_email = recipients.filter(user__groups__in=groups).distinct().values_list("user__email", flat=True)
+        subject = '%s: new patient registered' % settings.INSTALL_NAME.upper()
+        body = template.body
+        
+        try:
+            mass_email = []
+            for email in to_email:
+                mass_email.append( (subject, body, from_email, [email]) )
+        
+            send_mass_mail(mass_email)
+        except Exception, e:
+            print 'Error sending mail to user: ',to_email , ':', str(e)
 
 def getNewPatientEmailTemplate():
     #target=1 -> registry.configuration.models.EmailTemplate.TARGETS
-    return EmailTemplate.objects.get(target=1)
+    return EmailTemplate.objects.filter(target=1)
