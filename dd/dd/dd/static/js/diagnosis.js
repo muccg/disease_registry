19ca32b5cd18af8c9bc562e17d0025ce5f023a4f
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-07-13 12:24:23 +0800

    js linting

--- dd/dd/dd/static/js/diagnosis.js
+++ dd/dd/dd/static/js/diagnosis.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.field-treatment_other').hide();
     $('.field-condition_other').hide();
-    
+
     if ($('#id_treatmentcourse_set-0-treatment option:selected').text() == 'Other') {
         $('.field-treatment_other').toggle('fast');
     }
@@ -9,20 +9,20 @@ $(document).ready(function() {
     if ($('#id_diagnosis option:selected').text() == 'Other') {
         $('.field-condition_other').toggle('fast');
     }
-    
+
     $('#id_treatmentcourse_set-0-treatment').on('change', function() {
         if ($(this).find("option:selected").text() == 'Other') {
             $('.field-treatment_other').show('fast');
         } else {
             $('.field-treatment_other').hide('fast');
         }
-    })
-    
+    });
+
     $('#id_diagnosis').on('change', function() {
         if ($(this).find("option:selected").text() == 'Other') {
             $('.field-condition_other').show('fast');
         } else {
             $('.field-condition_other').hide('fast');
         }
-    })
+    });
 });
