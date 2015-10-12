af560f6f33a77e63d0f0b1b4871d0d0e65144010
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-10-12 17:21:07 +0800

    merge next_release

---
+++ dmd/dmd_db.py
@@ -0,0 +1,27 @@
+import pprint
+import json
+
+from django.db.models import get_app
+from django.db.models import get_models
+
+apps =["dmd", "genetic"]
+
+models = []
+
+for app_item in apps:
+    app = get_app(app_item)
+
+    for model in get_models(app):
+        m = {}
+        fields = []
+        for field in model._meta.fields:
+            fields.append(field.get_attname_column()[0])
+        m[model._meta.db_table.replace("_",".")] = fields
+        models.append(m)
+
+
+pprint.pprint(models)
+
+f = open("dmd.json", "w")
+f.write(json.dumps(models, indent=4))
+f.close()
