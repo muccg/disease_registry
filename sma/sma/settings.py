d1adaf9fdb41f8a8cf5ac716f46cf292976c9510
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-10-13 16:57:58 +0800

    update sma settings

--- sma/sma/settings.py
+++ sma/sma/settings.py
@@ -3,37 +3,40 @@
 Django settings defaults
 """
 import os
+from ccg_django_utils.conf import EnvConfig
+
+env = EnvConfig()
+
+SCRIPT_NAME = env.get("script_name", os.environ.get("HTTP_SCRIPT_NAME", ""))
+FORCE_SCRIPT_NAME = env.get("force_script_name", "") or SCRIPT_NAME or None
 
 WEBAPP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # General site config
-DEBUG = True
-DEV_SERVER = True
+PRODUCTION = env.get("production", False)
+DEBUG = env.get("debug", not PRODUCTION)
+DEV_SERVER = env.get("dev_server", not PRODUCTION)
 SITE_ID = 1
-APPEND_SLASH = True
-SSL_ENABLED = False
+APPEND_SLASH = env.get("append_slash", True)
+SSL_ENABLED = env.get("ssl_enabled", PRODUCTION)
 
+ROOT_URLCONF = env.get("root_urlconf", 'sma.urls')
+
+SECRET_KEY = env.get("secret_key", "changeme")
 
 # Locale
 TIME_ZONE = 'Australia/Perth'
 LANGUAGE_CODE = 'en-us'
 USE_I18N = True
 
-SESSION_COOKIE_NAME = "registry_sma"
-CSRF_COOKIE_NAME = "csrftoken_registry_sma"
-
-ROOT_URLCONF = 'sma.urls'
-SITE_NAME = 'sma'
-SECRET_KEY = '5d0ac501312a26afbc0d857af99ea556'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'registryapp',
-        'NAME': 'sma',
-        'PASSWORD': 'registryapp',
-        'HOST': 'db',
-        'PORT': '',
+        'USER': env.get("dbuser", "registryapp"),
+        'NAME': env.get("dbname", "sma"),
+        'PASSWORD': env.get("dbpass", "registryapp"),
+        'HOST': env.get("dbserver", "db"),
+        'PORT': env.get("dbport", ""),
     }
 }
 
@@ -45,13 +48,11 @@ TEMPLATE_LOADERS = [
 # see: https://docs.djangoproject.com/en/dev/ref/settings/#middleware-classes
 MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
-#    'iprestrict.middleware.IPRestrictMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'ccg.middleware.ssl.SSLRedirect',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'registry.common.middleware.TimeLogMiddleware'
 ]
 
 # see: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -71,13 +72,15 @@ INSTALLED_APPS = [
     'registry.genetic',
     'registry.common',
     'registry.configuration',
-    'sma.sma',
     'django.contrib.admin',
     'admin_views',
     'reversion',
     'iprestrict',
-    'explorer'
+    'explorer',
 ]
+
+# this needs to contain something like 'sma.sma' at a minimum
+INSTALLED_APPS.extend(env.getlist("installed_apps"))
 
 # these determine which authentication method to use
 # apps use modelbackend by default, but can be overridden here
@@ -87,37 +90,41 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # email
-EMAIL_HOST = 'ccg.murdoch.edu.au'
-SERVER_EMAIL = "apache@ccg.murdoch.edu.au"  # from address
-RETURN_EMAIL = 'web@ccg.murdoch.edu.au'
+EMAIL_USE_TLS = env.get("email_use_tls", False)
+EMAIL_HOST = env.get("email_host", 'smtp')
+EMAIL_PORT = env.get("email_port", 25)
+EMAIL_APP_NAME = env.get("email_app_name", "Registry {0}".format(SCRIPT_NAME))
+EMAIL_SUBJECT_PREFIX = env.get("email_subject_prefix", "DEV {0}".format(SCRIPT_NAME))
 
 # default emails
 ADMINS = [
-    ('Tech Alerts', 'alerts@ccg.murdoch.edu.au')
+    ('alerts', env.get("alert_email", "root@localhost"))
 ]
 MANAGERS = ADMINS
 
 
 STATIC_ROOT = os.path.join(WEBAPP_ROOT, 'static')
-STATIC_URL = '{0}/static/'.format(os.environ.get("SCRIPT_NAME", ""))
+STATIC_URL = '{0}/static/'.format(SCRIPT_NAME)
 
 MEDIA_ROOT = os.path.join(WEBAPP_ROOT, 'media')
-MEDIA_URL = '{0}/static/media/'.format(os.environ.get("SCRIPT_NAME", ""))
+MEDIA_URL = '{0}/static/media/'.format(SCRIPT_NAME)
 
 # for local development, this is set to the static serving directory. For deployment use Apache Alias
 STATIC_SERVER_PATH = os.path.join(WEBAPP_ROOT, "static")
 
 # a directory that will be writable by the webserver, for storing various files...
-WRITABLE_DIRECTORY = "/tmp"
+WRITABLE_DIRECTORY = env.get("writable_directory", "/tmp")
 TEMPLATE_DEBUG = DEBUG
 
 # session and cookies
-SESSION_COOKIE_AGE = 60 * 60
-SESSION_COOKIE_PATH = '{0}/'.format(os.environ.get("SCRIPT_NAME", ""))
-SESSION_SAVE_EVERY_REQUEST = True
-CSRF_COOKIE_NAME = "csrftoken_registry"
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_AGE = env.get("session_cookie_age", 60 * 60)
+SESSION_COOKIE_PATH = '{0}/'.format("SCRIPT_NAME")
+SESSION_SAVE_EVERY_REQUEST = env.get("session_save_every_request", True)
+SESSION_COOKIE_HTTPONLY = env.get("session_cookie_httponly", True)
+SESSION_COOKIE_SECURE = env.get("session_cookie_secure", PRODUCTION)
+SESSION_COOKIE_NAME = env.get("session_cookie_name", "registry_{0}".format(SCRIPT_NAME.replace("/", "")))
+
+CSRF_COOKIE_NAME = env.get("csrf_cookie_name", "csrf_{0}".format(SESSION_COOKIE_NAME))
 
 # see https://docs.djangoproject.com/en/dev/ref/settings/#session-engine
 # https://docs.djangoproject.com/en/1.3/ref/settings/#std:setting-SESSION_FILE_PATH
@@ -125,15 +132,36 @@ SESSION_COOKIE_SECURE = False
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 SESSION_FILE_PATH = WRITABLE_DIRECTORY
 
+# Testing settings
+INSTALLED_APPS.extend(['django_nose'])
+#TEST_RUNNER = 'sma.sma.tests.PatchedNoseTestSuiteRunner'
+SOUTH_TESTS_MIGRATE = True
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-erase',
+    '--cover-html',
+    '--cover-branches',
+    '--cover-package=sma',
+]
 
 # APPLICATION SPECIFIC SETTINGS
 AUTH_PROFILE_MODULE = 'groups.User'
-EMAIL_APP_NAME = "Registry "
+
+if env.get("memcache", ""):
+    # memcache server list
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': env.getlist("memcache"),
+            'KEYSPACE': env.get("keyspace", "registry-prod")
+        }
+    }
+    CACHE_BACKEND = 'memcached://'+(';'.join(env.getlist("memcache")))+"/"
 
 # #
 # # LOGGING
 # #
-LOG_DIRECTORY = os.path.join(WEBAPP_ROOT, "log")
+LOG_DIRECTORY = env.get('log_directory', os.path.join(WEBAPP_ROOT, "log"))
 try:
     if not os.path.exists(LOG_DIRECTORY):
         os.mkdir(LOG_DIRECTORY)
@@ -154,9 +182,6 @@ LOGGING = {
         'simple': {
             'format': 'Registry %(levelname)s %(message)s'
         },
-        'timelog': {
-            'format': '%(asctime)s %(message)s',
-        }
     },
     'filters': {
     },
@@ -196,15 +221,7 @@ LOGGING = {
             'class': 'django.utils.log.AdminEmailHandler',
             'formatter':'verbose',
             'include_html':True
-        },
-        'timelog': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(LOG_DIRECTORY, 'timelog.log'),
-            'formatter': 'timelog',
-            'maxBytes': 1024*1024*5,
-            'backupCount': 5,
-        },
+        }
     },
     'root': {
             'handlers':['console', 'errorfile', 'mail_admins'],
@@ -221,11 +238,6 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': False,
         },
-        'registry.common.middleware': {
-            'handlers': ['timelog'],
-            'level': 'DEBUG',
-            'propagate': False,
-        }
     }
 }
 
@@ -239,12 +251,14 @@ EXPLORER_SQL_WHITELIST = ('UPDATE',)
 EXPLORER_PERMISSION_VIEW = lambda u: u.is_superuser
 EXPLORER_PERMISSION_CHANGE = lambda u: u.is_superuser
 
-ALLOWED_HOSTS = [
-    'localhost'
-]
+INTERNAL_IPS = ('127.0.0.1', '172.16.2.1')
 
-INSTALL_NAME = 'sma'
+ALLOWED_HOSTS = env.getlist("allowed_hosts", ["localhost"])
+
+INSTALL_NAME = env.get("install_name", 'sma')
+
 QBE_ACCESS_FOR = lambda user: user.is_superuser
+LOGIN_URL = '{0}/admin'.format(SCRIPT_NAME)
 
 try:
     print "Attempting to import default settings as appsettings.sma"
